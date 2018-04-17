09a9282814ba9f5371905d5bc3636c55521889f5
aquarianmonkey <bcokee@gmail.com>
2018-04-17 09:05:17 +0800

    Renamed directory

---
+++ src/test/java/app/MyBootApplicationTests.java
@@ -0,0 +1,26 @@
+package app;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { MyBootApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
+public class MyBootApplicationTests {
+
+	private static final String API_ROOT
+			= "http://localhost:8081/api/books";
+
+	@Before
+	public void setUp() {
+	}
+
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
